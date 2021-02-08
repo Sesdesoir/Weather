5019b094b60d9a7dd82fa4dfd4cbfe36f56860dc
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-08 00:34:14 -0800

    Update script.js
    Basically made the entire thing modulatory...that is not the right word. Dynamic...I made it dynamic.

--- script.js
+++ script.js
@@ -2,11 +2,6 @@
 var histoyDiv   = document.getElementById("search-history");
 var todayDiv    = document.getElementById("today");
 var futureDiv   = document.getElementById("future-div");
-var futureCard1 = document.getElementById("future1");
-var futureCard2 = document.getElementById("future2");
-var futureCard3 = document.getElementById("future3");
-var futureCard4 = document.getElementById("future4");
-var futureCard5 = document.getElementById("future5");
 var searchButton = document.getElementById("search-button");
 var todayHeader = document.getElementById("cityNPic");
 var searchEntry = document.getElementById("search-entry").value;
@@ -28,6 +23,95 @@ function getWeather(event){
     var previousSearches = document.createElement("button");
     previousSearches.classList.add("previousSearch");
     previousSearches.textContent = document.getElementById("search-entry").value;
+    previousSearches.addEventListener("click", function(){
+        clearElements();
+    var latitude;
+    var longitude;
+    searchEntry = this.textContent;
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+ searchEntry +"&mode=json&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(response){
+        if(response.ok){
+            response.json().then(function(apiData){
+            console.log(apiData);
+            latitude = apiData.coord.lat;
+            longitude = apiData.coord.lon;
+            //created html elements
+            var weatherimg = document.createElement("img"); 
+             weatherimg.src = "http://openweathermap.org/img/wn/" + apiData.weather[0].icon + "@2x.png";
+             weatherimg.classList.add("weatherIcon");
+            var City = document.createElement("h1");
+            City.classList.add("city");
+            City.innerHTML = apiData.name + " ";
+            var tempToday = document.createElement("p");
+            tempToday.innerHTML ="<strong>Temperature:</strong> " + apiData.main.temp + " deg F";
+            var humidityToday = document.createElement("p");
+            humidityToday.innerHTML ="<strong>Humidity:<strong> " + apiData.main.humidity + "%";
+            var windSpeedToday = document.createElement("p");
+            windSpeedToday.innerHTML = "<strong>Wind Speed:<strong> " + apiData.wind.speed + " MPH";
+            //append the children
+            todayHeader.appendChild(City);
+            todayHeader.appendChild(weatherimg)
+            todayDiv.appendChild(tempToday);
+            todayDiv.appendChild(humidityToday)
+            todayDiv.appendChild(windSpeedToday);
+            }).then(function(){
+                fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="+ longitude +"&exclude=minutely,hourly&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(respond){
+                 if(respond.ok){
+                     respond.json().then(function(data){
+                         var uvIndexToday = document.createElement("p");
+                         uvIndexToday.innerHTML = "<strong>UV Index:</strong> " + data.current.uvi;
+                         if(data.current.uvi < 3){
+                             uvIndexToday.classList.add("lowRisk");
+                         }else if(data.current.uvi <6){
+                             uvIndexToday.classList.add("ModerateRisk");
+                         }else if(data.current.uvi <8){
+                             uvIndexToday.classList.add("highRisk");
+                         }else{
+                             uvIndexToday.classList.add("extremeRisk");
+                         }
+                         todayDiv.appendChild(uvIndexToday);
+                         var forcastHeader = document.createElement("h1");
+                         forcastHeader.textContent = "5 Day Forecast";
+                         futureDiv.appendChild(forcastHeader);
+                         var cardGroup = document.createElement("div");
+                         cardGroup.classList.add("card-group")
+                         futureDiv.appendChild(cardGroup);
+                         //Card Creation
+                         for(var j=0; j<5; j++){
+                         var card = document.createElement("div");
+                         card.classList.add("card");
+                         card.style = "width: 18rem;";
+                         var cardBody = document.createElement("div");
+                         cardBody.classList.add("card-body");
+                         var cardHead = document.createElement("h5");
+                         cardHead.classList.add("card-title");
+                         cardHead.textContent = data.daily[j].dt;
+                         var futureweatherimg = document.createElement("img"); 
+                         futureweatherimg.src = "http://openweathermap.org/img/wn/" + data.daily[j].weather[0].icon + "@2x.png";
+                         futureweatherimg.classList.add("futureweatherIcon");
+                         var futureTemp = document.createElement("p");
+                         futureTemp.innerHTML = "<strong>Temp:</strong> " + data.daily[j].temp.day + " F";
+                         var futureHumidity = document.createElement("p");
+                         futureHumidity.innerHTML = "<strong>Humidity:</strong> " + data.daily[j].humidity + "%";
+                         //card appendage
+                         cardGroup.appendChild(card);
+                         card.appendChild(cardBody);
+                         cardBody.appendChild(cardHead);
+                         cardBody.appendChild(futureweatherimg);
+                         cardBody.appendChild(futureTemp);
+                         cardBody.appendChild(futureHumidity);
+                         }
+                         //testing
+                         console.log("Second Call:");
+                         console.log(data);
+                     }) //End data.then
+                 //End if .ok    
+                 } 
+                })
+             //End 2nd call     
+             })
+        } //end if
+    })
+    });
     histoyDiv.appendChild(previousSearches);              
     fetch("http://api.openweathermap.org/data/2.5/weather?q="+ searchEntry +"&mode=json&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(response){
         if(response.ok){
@@ -58,15 +142,70 @@ function getWeather(event){
             console.log("icon id: "+apiData.weather[0].icon);
             console.log("Latitude: "+ latitude);
             console.log("Longitude: "+ longitude);
-        })/*.then(function(){
-           fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="+ longitude +"&exclude=minutely,hourly&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72")  
-        })*/
+            //second fetch starts here
+        }).then(function(){
+           fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="+ longitude +"&exclude=minutely,hourly&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(respond){
+            if(respond.ok){
+                respond.json().then(function(data){
+                    var uvIndexToday = document.createElement("p");
+                    uvIndexToday.innerHTML = "<strong>UV Index:</strong> " + data.current.uvi;
+                    if(data.current.uvi < 3){
+                        uvIndexToday.classList.add("lowRisk");
+                    }else if(data.current.uvi <6){
+                        uvIndexToday.classList.add("ModerateRisk");
+                    }else if(data.current.uvi <8){
+                        uvIndexToday.classList.add("highRisk");
+                    }else{
+                        uvIndexToday.classList.add("extremeRisk");
+                    }
+                    todayDiv.appendChild(uvIndexToday);
+                    var forcastHeader = document.createElement("h1");
+                    forcastHeader.textContent = "5 Day Forecast";
+                    futureDiv.appendChild(forcastHeader);
+                    var cardGroup = document.createElement("div");
+                    cardGroup.classList.add("card-group")
+                    futureDiv.appendChild(cardGroup);
+                    //Card Creation
+                    for(var j=0; j<5; j++){
+                    var card = document.createElement("div");
+                    card.classList.add("card");
+                    card.style = "width: 18rem;";
+                    var cardBody = document.createElement("div");
+                    cardBody.classList.add("card-body");
+                    var cardHead = document.createElement("h5");
+                    cardHead.classList.add("card-title");
+                    cardHead.textContent = data.daily[j].dt;
+                    var futureweatherimg = document.createElement("img"); 
+                    futureweatherimg.src = "http://openweathermap.org/img/wn/" + data.daily[j].weather[0].icon + "@2x.png";
+                    futureweatherimg.classList.add("futureweatherIcon");
+                    var futureTemp = document.createElement("p");
+                    futureTemp.innerHTML = "<strong>Temp:</strong> " + data.daily[j].temp.day;
+                    var futureHumidity = document.createElement("p");
+                    futureHumidity.innerHTML = "<strong>Humidity:</strong> " + data.daily[j].humidity;
+                    //card appendage
+                    cardGroup.appendChild(card);
+                    card.appendChild(cardBody);
+                    cardBody.appendChild(cardHead);
+                    cardBody.appendChild(futureweatherimg);
+                    cardBody.appendChild(futureTemp);
+                    cardBody.appendChild(futureHumidity);
+                    }
+                    //testing
+                    console.log("Second Call:");
+                    console.log(data);
+                }) //End data.then
+            //End if .ok    
+            } 
+           })
+        //End 2nd call     
+        })
     }
 // End first fetch
 })
 
 //end function
 }
+
 
 function clearElements (){
     if(todayHeader !== null){
@@ -75,8 +214,7 @@ function clearElements (){
     if(todayDiv !== null){
         todayDiv.innerHTML = "";
     }
-    for(var i=1; i<=5; i++){
-        if(("futureCard"+i) !== null)    
-        ("futureCard" + i).innerHTML = "";
+    if(futureDiv !== null){
+        futureDiv.innerHTML = ""
     }
 }
