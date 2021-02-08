13a19cf8e4b05f8ad0b7c2315028b34799bee6b9
Leslie Williams <71472699+Sesdesoir@users.noreply.github.com>
2021-02-07 22:51:06 -0800

    Update script.js
    first api call working. elements append beautifully, with icon. Clear function working for new searches. Buttons are being added to history.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var futureCard3 = document.getElementById("future3");
 var futureCard4 = document.getElementById("future4");
 var futureCard5 = document.getElementById("future5");
 var searchButton = document.getElementById("search-button");
-
+var todayHeader = document.getElementById("cityNPic");
 var searchEntry = document.getElementById("search-entry").value;
 
 searchButton.addEventListener("click", getWeather);
@@ -19,28 +19,63 @@ searchButton.addEventListener("click", getWeather);
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&units=imperial&appid={API key}
 
 function getWeather(event){
+    event.preventDefault();
+    clearElements();
     var latitude;
     var longitude;
-    event.preventDefault();
     searchEntry = document.getElementById("search-entry").value;
-    fetch("http://api.openweathermap.org/data/2.5/weather?q="+ "suisun" +"&mode=json&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(response){
+    localStorage.setItem("City", document.getElementById("search-entry").value );
+    var previousSearches = document.createElement("button");
+    previousSearches.classList.add("previousSearch");
+    histoyDiv.appendChild(previousSearches);              
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+ searchEntry +"&mode=json&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72").then(function(response){
         if(response.ok){
             response.json().then(function(apiData){
             console.log(apiData);
-            latitude = apiData.lat;
-            longitude = apiData.lon;
+            latitude = apiData.coord.lat;
+            longitude = apiData.coord.lon;
             //created html elements
             var weatherimg = document.createElement("img"); 
              weatherimg.src = "http://openweathermap.org/img/wn/" + apiData.weather[0].icon + "@2x.png";
+             weatherimg.classList.add("weatherIcon");
             var City = document.createElement("h1");
-            City.textContent = apiData.name + " " + todaysDate.text(moment().format('LL')) + weatherimg;
-            var 
-        }).then(function(){
+            City.classList.add("city");
+            City.innerHTML = apiData.name + " ";
+            var tempToday = document.createElement("p");
+            tempToday.innerHTML ="<strong>Temperature:</strong> " + apiData.main.temp + " deg F";
+            var humidityToday = document.createElement("p");
+            humidityToday.innerHTML ="<strong>Humidity:<strong> " + apiData.main.humidity + "%";
+            var windSpeedToday = document.createElement("p");
+            windSpeedToday.innerHTML = "<strong>Wind Speed:<strong> " + apiData.wind.speed + " MPH";
+            //append the children
+            todayHeader.appendChild(City);
+            todayHeader.appendChild(weatherimg)
+            todayDiv.appendChild(tempToday);
+            todayDiv.appendChild(humidityToday)
+            todayDiv.appendChild(windSpeedToday);
+            //variable test
+            console.log("icon id: "+apiData.weather[0].icon);
+            console.log("Latitude: "+ latitude);
+            console.log("Longitude: "+ longitude);
+        })/*.then(function(){
            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="+ longitude +"&exclude=minutely,hourly&units=imperial&appid=2616b5aba9cde5c3435e1b66d6740d72")  
-        })
+        })*/
     }
 // End first fetch
 })
 
 //end function
+}
+
+function clearElements (){
+    if(todayHeader !== null){
+        todayHeader.innerHTML = "";
+    }
+    if(todayDiv !== null){
+        todayDiv.innerHTML = "";
+    }
+    for(var i=1; i<=5; i++){
+        if(("futureCard"+i) !== null)    
+        ("futureCard" + i).innerHTML = "";
+    }
 }
